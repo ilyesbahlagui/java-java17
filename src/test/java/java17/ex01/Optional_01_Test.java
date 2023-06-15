bdfc95e74fa8a5de1ec9e8b9752355f314e5a96e
ilyes_3 <ilyesbahlagui@gmail.com>
2023-06-15 16:30:17 +0200

    tp4

--- src/test/java/java17/ex01/Optional_01_Test.java
+++ src/test/java/java17/ex01/Optional_01_Test.java
@@ -1,6 +1,5 @@
 package java17.ex01;
 
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -16,35 +15,41 @@ import java17.data.Person;
  * Exercice 02 - Filter, Map
  */
 public class Optional_01_Test {
-	
+
 	class NotPresentException extends RuntimeException {
-		
+
 	}
 
-    @Test
-    public void test_optional_ifPresent() throws Exception {
-    	
-    	List<Person> persons = Data.buildPersonList(100);
+	@Test
+	public void test_optional_ifPresent() throws Exception {
 
-        // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 18 ans
-        // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
-        assertThat(optPerson.isPresent(), is(true));
-        
-        // TODO afficher la personne en question si l'optional contient une personne
-    }
+		List<Person> persons = Data.buildPersonList(100);
 
-    @Test(expected=NotPresentException.class)
-    public void test_optional_notPresent() throws Exception {
-    	List<Person> persons = Data.buildPersonList(50);
+		// TODO rechercher dans la liste ci-dessus la 1ère personne ayant 18 ans
+		// TODO utiliser la méthode "findFirst"
+		Optional<Person> optPerson = persons.stream().filter(p -> p.getAge() == 18).findFirst();
+		assertThat(optPerson.isPresent(), is(true));
 
-        // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 75 ans
-        // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
-        assertThat(optPerson.isPresent(), is(false));
-        
-        // TODO si la personne n'existe pas, jeter une exception NotPresentException
-        // TODO utiliser la méthode "orElseThrow"
+		// TODO afficher la personne en question si l'optional contient une personne
+		if (optPerson.isPresent()) {
+			Person person = optPerson.get();
+			System.out.println(person);
+		}
+	}
 
-    }
+	@Test(expected = NotPresentException.class)
+	public void test_optional_notPresent() throws Exception {
+		List<Person> persons = Data.buildPersonList(50);
+
+		// TODO rechercher dans la liste ci-dessus la 1ère personne ayant 75 ans
+		// TODO utiliser la méthode "findFirst"
+		Optional<Person> optPerson = persons.stream().filter(p -> p.getAge() == 75).findFirst();
+		assertThat(optPerson.isPresent(), is(false));
+
+		// TODO si la personne n'existe pas, jeter une exception NotPresentException
+		// TODO utiliser la méthode "orElseThrow"
+
+		optPerson.orElseThrow(NotPresentException::new);
+
+	}
 }
