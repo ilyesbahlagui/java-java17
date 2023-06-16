e9de6a5e6b4ca2cff464e6512eb1abac1b46b1c3
ilyes_3 <ilyesbahlagui@gmail.com>
2023-06-16 12:28:27 +0200

    tp6

--- src/test/java/java17/ex01/DateAndTime_01_Test.java
+++ src/test/java/java17/ex01/DateAndTime_01_Test.java
@@ -18,12 +18,13 @@ public class DateAndTime_01_Test {
 	public void test_date() {
 
 		// TODO modifier les paramètres du constructeur pour que le test soit passant
-		Date date = new Date(0, 0, 0, 0, 0, 0);
+		Date date = new Date(2017 - 1900, 2, 24, 1, 2, 3);
 
 		assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
 		// TODO modifier le jour de la date avec une méthode set pour que le test soit
 		// passant
+		 date.setTime(date.getTime() + 24 * 60 * 60 * 1000L);
 
 		assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
 	}
@@ -35,12 +36,18 @@ public class DateAndTime_01_Test {
 		Calendar calendar = Calendar.getInstance();
 
 		// TODO modifier l'objet calendar pour que le test soit passant
+        calendar.set(Calendar.YEAR, 2017);
+        calendar.set(Calendar.MONTH, Calendar.MARCH);
+        calendar.set(Calendar.DAY_OF_MONTH, 24);
+        calendar.set(Calendar.HOUR_OF_DAY, 1);
+        calendar.set(Calendar.MINUTE, 2);
+        calendar.set(Calendar.SECOND, 3);
 
 		assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
 		// calendar est mutable... :-(
 		// TODO modifier l'objet calendar pour que le test soit passant
-
+		 calendar.add(Calendar.DAY_OF_MONTH, 1);
 		assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
 	}
 
